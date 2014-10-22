5ddc2f8c5ae49e9c4e0c5af2fa9813b126476659
James Daly <james@atomsplitgames.com>
2014-10-22 15:56:54 +0100

    added sequencing

--- Classes/testingScene.cpp
+++ Classes/testingScene.cpp
@@ -9,6 +9,31 @@ bool TestingScene::init()
 		return false;
 	}
 
+	m_fScreenWidth  = CCDirector::sharedDirector()->getWinSize().width;
+	m_fScreenHeight = CCDirector::sharedDirector()->getWinSize().height;
+
+	//use the create keyword to initialize cocos
+	//objects
+	mp_oMainLayer = CCLayer::create();
+	mp_oPowerButton = CCSprite::create("CloseSelected.png");
+	
+	//set position to be the middle of the screen
+	mp_oPowerButton->setPosition(ccp(m_fScreenWidth/2, m_fScreenHeight/2));
+
+	this->addChild(mp_oMainLayer);
+	mp_oMainLayer->addChild(mp_oPowerButton);
+
+	CCMoveTo* moveTopCorner = CCMoveTo::create(0.5f, ccp(m_fScreenWidth, m_fScreenHeight));
+	CCMoveTo* moveBottomCorner = CCMoveTo::create(0.5f, ccp(0, 0));
+	
+	CCSequence* topbotrepeat = CCSequence::create(moveBottomCorner, moveTopCorner, NULL);
+
+	mp_oPowerButton->runAction(topbotrepeat);
+
+
+
+
+
 	return true;
 }
 
--- Classes/testingScene.h
+++ Classes/testingScene.h
@@ -3,6 +3,8 @@
 
 #include "cocos2d.h"
 
+USING_NS_CC;
+
 class TestingScene : public cocos2d::CCScene
 {
 public:
@@ -10,7 +12,18 @@ public:
 
 
 
+
 	CREATE_FUNC(TestingScene);
+
+private:
+	//create a layer to add sprites to 
+	CCLayer* mp_oMainLayer;
+	CCSprite* mp_oPowerButton;
+	
+	float m_fScreenWidth;
+	float m_fScreenHeight;
+
+
 };
 
 
